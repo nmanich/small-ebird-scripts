6a2c4399f25189fd5fd9373eb302186068ad3211
Nick Anich <47158373+nmanich@users.noreply.github.com>
2019-11-25 20:33:41 -0600

    Update exports_observations_from_checklists_that_have_no_codes.R

--- exports_observations_from_checklists_that_have_no_codes.R
+++ exports_observations_from_checklists_that_have_no_codes.R
@@ -2,6 +2,8 @@
 #Thanks to Nick Walton for this
 #May want to filter by portal and also check for nocturnals
 
+# This version should work with the raw ebird txt file
+
 library(data.table)
 library(parallel)
 
@@ -14,6 +16,21 @@ sp <- sp[sp[, if(all(BREEDING.BIRD.ATLAS.CATEGORY %in% no_code)) .I,
             by = SAMPLING.EVENT.IDENTIFIER]$V1]
 
 fwrite(sp, file = "myfile.txt", sep = "\t", nThread = n_core)
+
+# This version should work with the postgres csv output
+
+library(data.table)
+library(parallel)
+
+n_core <- detectCores() - 1
+sp <- fread("ALLATLASWISEPT19_2.csv", sep = ",",
+            nThread = n_core, check.names = TRUE)
+
+no_code <- c("C1", "")
+sp <- sp[sp[, if(all(breeding_bird_atlas_category %in% no_code)) .I,
+            by = sampling_event_identifier]$V1]
+
+fwrite(sp, file = "ALLATLASWISEPT19_6.txt", sep = "\t", nThread = n_core)
 
 
 
