4efae8f43b51e66de223fe15e00d7e65ae90a272
Nick Anich <47158373+nmanich@users.noreply.github.com>
2023-09-11 22:00:34 -0500

    added list function to column names

--- column-name-fixing.R
+++ column-name-fixing.R
@@ -16,3 +16,9 @@ names(df) <- toupper(names(df))
 
 library(stringr)   
 colnames(df) <- str_replace_all(colnames(df), "[:punct:]", ".")
+
+#####################################################
+#Case 3: From snake_case to UPPERCASE.SPACE.AS.PERIOD where sp is a list function
+
+sp <- lapply(sp, function (y) {colnames(y) <- gsub("\\_", "\\.", colnames(y)); y})
+sp <- lapply(sp, function (x) dplyr::rename_with(x, toupper))
