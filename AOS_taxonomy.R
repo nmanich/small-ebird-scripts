4410b40f6a65665b92d0a1f54d046f0264e785b6
Nick Anich <47158373+nmanich@users.noreply.github.com>
2024-07-05 09:28:32 -0500

    Update AOS_taxonomy.R

--- AOS_taxonomy.R
+++ AOS_taxonomy.R
@@ -1,6 +1,6 @@
 # These taxa are about to change in eBird and have not yet been changed by AOS. To keep things in AOS taxonomy, then:
 
-ebird$COMMON.NAME <- gsub("Western Cattle Egret", "Cattle Egret", ebird$COMMON.NAME)
+ebird$COMMON.NAME <- gsub("Western Cattle-Egret", "Cattle Egret", ebird$COMMON.NAME)
 ebird$COMMON.NAME <- gsub("Yellow-crowned Night Heron", "Yellow-crowned Night-Heron", ebird$COMMON.NAME)
 ebird$COMMON.NAME <- gsub("Black-crowned Night Heron", "Black-crowned Night-Heron", ebird$COMMON.NAME)
 
