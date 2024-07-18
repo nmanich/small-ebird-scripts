cc6e5e53cf4d08b2da991dc61473009d3ba50cfb
Nick Anich <47158373+nmanich@users.noreply.github.com>
2024-07-18 09:15:22 -0500

    Update AOS_taxonomy.R

--- AOS_taxonomy.R
+++ AOS_taxonomy.R
@@ -3,12 +3,12 @@
 ebird$COMMON.NAME <- gsub("Western Cattle-Egret", "Cattle Egret", ebird$COMMON.NAME)
 ebird$COMMON.NAME <- gsub("Yellow-crowned Night Heron", "Yellow-crowned Night-Heron", ebird$COMMON.NAME)
 ebird$COMMON.NAME <- gsub("Black-crowned Night Heron", "Black-crowned Night-Heron", ebird$COMMON.NAME)
+ebird$COMMON.NAME <- gsub("American Herring Gull", "Herring Gull", ebird$COMMON.NAME)
 
 To bring early 2023 file up to AOS taxonomy, then:
 
 ebird$COMMON.NAME <- gsub("Northern Goshawk", "American Goshawk", ebird$COMMON.NAME)
 ebird$COMMON.NAME <- gsub("House Wren", "Northern House Wren", ebird$COMMON.NAME)
-ebird$COMMON.NAME <- gsub("Herring Gull", "American Herring Gull", ebird$COMMON.NAME)
 ebird$COMMON.NAME <- gsub("Barn Owl", "American Barn Owl", ebird$COMMON.NAME)
 
 
