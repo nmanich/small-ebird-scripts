8fd86a48242581a9588ede22b8e14fbd005eecfa
Nick Anich <47158373+nmanich@users.noreply.github.com>
2026-07-06 18:55:53 -0500

    Update extract-SGCN.R

--- extract-SGCN.R
+++ extract-SGCN.R
@@ -20,5 +20,5 @@ write.csv(SGCN, file= "2026SGCN.csv", row.names=FALSE)
 SGCNminus5 <- ebird[grepl("American Black Duck|Common Goldeneye|Sharp-tailed Grouse|Greater Prairie-Chicken|Spruce Grouse|Common Nighthawk|Eastern Whip-poor-will|King Rail|Yellow Rail|Whooping Crane|Black-necked Stilt|Piping Plover|Upland Sandpiper|Wilson's Phalarope|Caspian Tern|Black Tern|Forster's Tern|Common Tern|Red-necked Grebe|Black-crowned Night-Heron|Northern Goshawk|Long-eared Owl|Short-eared Owl|Black-backed Woodpecker|Peregrine Falcon|Olive-sided Flycatcher|Bell's Vireo|Loggerhead Shrike|Canada Jay|Boreal Chickadee|Ruby-crowned Kinglet|Swainson's Thrush|Evening Grosbeak|Grasshopper Sparrow|LeConte's Sparrow|Nelson's Sparrow|Henslow's Sparrow|Yellow-breasted Chat|Yellow-headed Blackbird|Western Meadowlark|Eastern Meadowlark|Brewer's Blackbird|Worm-eating Warbler|Golden-winged Warbler|Connecticut Warbler|Kentucky Warbler|Kirtland's Warbler|Cerulean Warbler|Yellow-throated Warbler", ebird$COMMON.NAME), ]
 
 # export to csv
-write.csv(SGCN, file= "2026SGCNminus5.csv", row.names=FALSE)
+write.csv(SGCNminus5, file= "2026SGCNminus5.csv", row.names=FALSE)
 
